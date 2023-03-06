025005a835be8d8ca86b995c6e17c62e2fbbaee7
mdanilenco <mihail.danilenco18@gmail.com>
2023-03-06 22:10:29 +0200

    Update Client Server application

--- src/ClientServerLab1/Client.java
+++ src/ClientServerLab1/Client.java
@@ -1,5 +1,6 @@
 package ClientServerLab1;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -7,32 +8,39 @@ import java.util.Scanner;
 
 public class Client {
     private DataOutputStream outputStream = null;
+    private DataInputStream inputStream = null;
     private Scanner scanner = null;
     private Socket socket = null;
 
     public Client(String address, int port) {
         String userInput = "";
+        String serverMessage = "";
+
         try {
             socket = new Socket(address, port);
             System.out.println("To terminate the session write: FINISH");
             scanner = new Scanner(System.in);
             outputStream = new DataOutputStream(socket.getOutputStream());
+            inputStream = new DataInputStream(socket.getInputStream());
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
 
         while (!userInput.equals("FINISH")) {
-            var input = scanner.nextLine();
+            userInput = scanner.nextLine();
             try {
-                outputStream.writeUTF(input);
+                outputStream.writeUTF(userInput);
+                serverMessage = inputStream.readUTF();
+                System.out.println(serverMessage);
             } catch (IOException e) {
-                System.out.println("Error: " + e);
+                System.out.println("Server is down. Please insert FINISH to terminate connection!");
             }
         }
-        System.out.println("Connection finished!");
 
+        System.out.println("Connection finished!");
         try {
             outputStream.close();
+            inputStream.close();
             socket.close();
         } catch (IOException e) {
             System.out.println("Error: " + e);
--- src/ClientServerLab1/README.md
+++ src/ClientServerLab1/README.md
@@ -1,1 +1,5 @@
 **Sarcina pentru lab1**
+- Run **main** from Server
+- Run **main** from Client
+- Write message in client's console
+- Verify sent message in server's console
